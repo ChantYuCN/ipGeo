28399bc6f71e003e7ed1674926b6506abf27a905
ChantYuCN <yuchander@gmail.com>
2020-10-14 13:30:32 +0800

    access the url and response the json for geo by giving ip

--- ipgeo/cli.py
+++ ipgeo/cli.py
@@ -1,17 +1,29 @@
 """Console script for ipgeo."""
 import argparse
 import sys
+from ipgeo import APIrequest
 
 
 def main():
     """Console script for ipgeo."""
     parser = argparse.ArgumentParser()
     parser.add_argument('_', nargs='*')
+    parser.add_argument("-v",
+                        "--verbosity",
+                        default=None,
+                        help="increase output verbosity.")
+    parser.add_argument("-i",
+                        "--ip",
+                        default=None,
+                        help="refer to given ip to locat the contry.")
     args = parser.parse_args()
 
     print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "ipgeo.cli.main")
+    print("Replace this message by putting your code into " "ipgeo.cli.main")
+
+    print(args)
+    APIrequest(args)
+
     return 0
 
 
--- ipgeo/ipgeo.py
+++ ipgeo/ipgeo.py
@@ -1,1 +1,22 @@
 """Main module."""
+from urllib.request import urlopen
+import json
+
+
+"""access the url and response the json for geo by giving ip"""
+def APIrequest(ipaddr):
+    urlIpGeo = 'http://ip-api.com/json/'
+    if ipaddr.ip:
+        """Temporary"""
+        print(ipaddr.ip)
+        urlIpGeo = 'http://ip-api.com/json/'
+    try:
+        resp = urlopen(urlIpGeo).read().decode('UTF-8')
+        json_obj = json.loads(resp)
+        return json_obj
+    except Exception as e:
+        print(e)
+        return 1
+
+
+# This is a new line that ends the file.
